c2ec70641c643f5c740fac329f51cc1f07f7dd4b
Anggara <anggara.roshandi.putra5@gmail.com>
2024-01-12 13:03:52 +0700

    style, color: update color in colors.dart

--- lib/app/utils/colors.dart
+++ lib/app/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TColors {
-  static const Color primaryColor = Color(0xFFD19F2A);
+  static const Color primaryColor = Color(0xFF1D232E);
   static const Color grayColor = Color(0xFFA1A8B0);
   static const Color textBodyColor = Color(0xFF353535);
   static const Color textLabelColor = Color(0xFFA1A8B0);
